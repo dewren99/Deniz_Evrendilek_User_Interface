0d4a2a12e475e9cca4bfaa5039995acad198e848
Deniz Evrendilek <devrendilek@gmail.com>
2023-09-25 17:16:14 -0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Deniz_Evrendilek_User_Interface"
+include(":app")
+ 
